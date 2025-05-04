386f6d6b43f0e5a33ddfb77daa68aee8d62e6a9d
Maryna <89296066105marina@gmal.com>
2025-05-04 13:28:38 +0200

    store

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import todoReducers from './todoSlice'
+
+export const store = configureStore({
+    reducer:{
+            todos: todoReducers,
+
+    }
+})
+
